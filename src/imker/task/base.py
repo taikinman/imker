ac4ca13bd835d3cdf9e494f5f6539ef00c7ac087
taikinman <okipedia6161@gmail.com>
2023-07-25 09:22:50 +0900

    update

--- src/imker/task/base.py
+++ src/imker/task/base.py
@@ -1,11 +1,16 @@
 import hashlib
 import pickle
 from typing import Any
+from abc import ABC, abstractmethod
 
 
-class BaseTask(object):
+class BaseTask(ABC):
     def fit(self, X: Any, y: Any = None):
         return self
+
+    @abstractmethod
+    def transform(self, X: Any):
+        pass
 
     def get_identifier(self, X):
         return hashlib.sha256(pickle.dumps(X)).hexdigest()
